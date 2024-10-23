3b47c448b0be3026eb8cde7ad2a590f16210d3d3
Karl Hoitsma <khoitsma@mathpax.com>
2024-10-23 12:50:19 -0500

    Update conf.py
    added "sphinx_rtd_theme" and options

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,27 @@ author = 'K. Hoitsma '
 html_logo = 'https://khoitsmahq.firstcloudit.com/images/mp_small3.png'
 # html_theme = 'sphinx13'
 
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    # 'logo_only': False,
+    'prev_next_buttons_location': 'top',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    'flyout_display': 'hidden',
+    'version_selector': True,
+    'language_selector': True,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 # source_suffix = {
 #     '.rst': 'restructuredtext',
 #     '.txt': 'restructuredtext',
